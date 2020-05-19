d22d34f6c1dd75a55a18f3bab38beed5c5fc1d6d
Gordiy <gordiy.yuriy.gord123@gmail.com>
2020-05-19 13:39:38 +0300

    Добавьте файлы проекта.

---
+++ EstateAgency/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstateAgency.Startup))]
+namespace EstateAgency
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
